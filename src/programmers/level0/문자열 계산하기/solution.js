8b0e5b244ebc7750225fb96003c6c07cc5d0d7b7
kbu715 <kbu715@gmail.com>
2023-03-18 23:07:38 +0900

    lv0 : 문자열 계산하기

---
+++ src/programmers/level0/문자열 계산하기/solution.js
@@ -0,0 +1,18 @@
+function solution(my_string) {
+  const stack = [];
+
+  let sign = 1;
+  for (const ch of my_string.split(" ")) {
+    if (ch === "+") {
+      sign = 1;
+    } else if (ch === "-") {
+      sign = -1;
+    } else {
+      stack.push(ch * sign);
+    }
+  }
+
+  console.log(stack);
+
+  return stack.reduce((a, b) => a + b, 0);
+}
