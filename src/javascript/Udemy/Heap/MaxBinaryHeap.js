3d6b3a4f67dd03c7d4f5bef689b3b545d8049fef
kbu715 <kbu715@gmail.com>
2023-03-30 16:10:57 +0900

    feat: Binary Heap (MaxBinaryHeap)

---
+++ src/javascript/Udemy/Heap/MaxBinaryHeap.js
@@ -0,0 +1,72 @@
+class MaxBinaryHeap {
+  constructor() {
+    this.values = [];
+  }
+  insert(element) {
+    this.values.push(element);
+    this.bubbleUp();
+  }
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+
+      if (element <= parent) break;
+
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+
+    return max;
+  }
+
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0]; // the element to sink down
+
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+
+      let leftChild, rightChild;
+      let swapIdx = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild > element) {
+          swapIdx = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swapIdx === null && rightChild > element) ||
+          (swapIdx !== null && rightChild > leftChild)
+        ) {
+          swapIdx = rightChildIdx;
+        }
+      }
+
+      if (swapIdx === null) break;
+
+      this.values[idx] = this.values[swapIdx];
+      this.values[swapIdx] = element;
+
+      idx = swapIdx;
+    }
+  }
+}
