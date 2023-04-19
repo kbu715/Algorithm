ffce6acdde2757e150bf9b01674ce0a944f1acd3
kbu715 <kbu715@gmail.com>
2023-04-19 16:08:27 +0900

    Graph Implementation

---
+++ src/javascript/Udemy/Graph/graph.js
@@ -0,0 +1,31 @@
+// Adjacency List
+// Undirected Graph
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertext(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
+
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
+
+  removeEdge(v1, v2) {
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter((v) => v !== v2);
+    this.adjacencyList[v2] = this.adjacencyList[v2].filter((v) => v !== v1);
+  }
+
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
+}
