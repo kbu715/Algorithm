955827a68b037d0a0819ef1c2e63ceb6450bcb1c
kbu715 <kbu715@gmail.com>
2023-04-19 17:51:10 +0900

    lv1 : 자연수 뒤집어 배열로 만들기

--- src/programmers/level1/자연수 뒤집어 배열로 만들기/solution.js
+++ src/programmers/level1/자연수 뒤집어 배열로 만들기/solution.js
@@ -1,9 +1,7 @@
 function solution(n) {
-  let answer = [];
-
-  let temp = n.toString().split('').map(v => parseInt(v)).reverse();
-
-  answer = temp;
-
-  return answer;
+  return n
+    .toString()
+    .split("")
+    .map((v) => +v)
+    .reverse();
 }
---
+++ src/programmers/level1/자연수 뒤집어 배열로 만들기/solution2.js
@@ -0,0 +1,14 @@
+function solution(n) {
+  // 문자풀이
+  // return (n+"").split("").reverse().map(v => parseInt(v));
+
+  // 숫자풀이
+  let arr = [];
+
+  do {
+    arr.push(n % 10);
+    n = Math.floor(n / 10);
+  } while (n > 0);
+
+  return arr;
+}
