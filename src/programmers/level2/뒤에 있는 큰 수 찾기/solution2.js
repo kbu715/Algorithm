fd2a4864c9b51f777e05f56e1d2151668cd2bc32
kbu715 <kbu715@gmail.com>
2023-05-06 13:36:35 +0900

    lv2 : 뒤에 있는 큰 수 찾기

---
+++ src/programmers/level2/뒤에 있는 큰 수 찾기/solution2.js
@@ -0,0 +1,14 @@
+function solution(numbers) {
+  let answer = Array(numbers.length);
+  let check = [0];
+  for (let i = 1; i < numbers.length; i++) {
+    while (check.length && numbers[check[check.length - 1]] < numbers[i]) {
+      answer[check.pop()] = numbers[i];
+    }
+    check.push(i);
+  }
+  while (check.length) {
+    answer[check.pop()] = -1;
+  }
+  return answer;
+}
